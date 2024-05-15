815c0d6eff0acdb5277a83f04ab2ede40f5a880c
LASmelov <smelov020@gmail.com>
2024-05-15 09:31:07 +0300

    0.7 add prisma script

--- app/User/TimeTable/page.js
+++ app/User/TimeTable/page.js
@@ -1,5 +1,5 @@
 'use client'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import {
 
     Table,
@@ -12,126 +12,123 @@ import {
 import { get } from 'http';
 
 export default function TimeTable() {
-    const [classroomId, setClassroomId] = useState(null);
-    const [data, setData] = useState(null); // добавьте это состояние для хранения данных
+    // const [classroomId, setClassroomId] = useState(null);
+    const [data, setData] = useState([]);
+    let day = 5
 
 
     const ClassRooms = [
         {
-            roomName: 'Аудитория 621',
+            roomName: '621-а',
             roomId: 210737156
         },
+
         {
-            roomName: 'Аудитория 623',
+            roomName: '623-б!',
             roomId: 217739053
         },
+
+        {
+            roomName: 'svarka!',
+            roomId: 218067665
+        },
+
     ]
 
-    async function getData() {
-        let data = []
-        for (let room of ClassRooms) {
-            const resp = await (await fetch(`https://edu.donstu.ru/api/Rasp?idAudLine=${room.roomId}&sdate=2024-05-06`)).json()
+
+
+    useEffect(() => {
+        async function getData() {
+            let data = []
+            for (let room of ClassRooms) {
+                const resp = await (await fetch(`https://edu.donstu.ru/api/Rasp?idAudLine=${room.roomId}&sdate=2024-05-06`)).json()
+                data.push({
+                    class: room.roomName,
+                    rasp: resp.data.rasp
+                });
+
+            }
+
+            console.table(data);
+            setData(data)
+        }
+        getData()
+    }, [])
+
+
+
+    function buildTable(data) {
+        let table = []
+        for (let i = 1; i < 8; i++) {
+
+            let tablePart = (
+                <TableRow>
+                    <TableCell>Пара {i}</TableCell>
+
+                    {
+                        data.map((aud) => {
+
+
+                            const lesson = aud.rasp.find(lesson => lesson.деньНедели === day && lesson.номерЗанятия === i);
+                            if (lesson) {
+                                return (
+                                    <TableCell key={aud.class}>
+                                        <p>{lesson.преподаватель}</p>
+                                        <p>{lesson.дисциплина}</p>
+                                        <p>{lesson.группа}</p>
+                                    </TableCell>
+                                )
+                            }
+                            else {
+                                return (<TableCell className="   gap-5" key={aud.class}>
+                                    <h1>Пары нету</h1>
+                                    <div className='bg-red-300 h-3 w-3 rounded-sm'> </div>
+                                </TableCell>)
+                            }
+                        })
+
+
+                    }
+
+                </TableRow>
+            )
+
+            table.push(tablePart)
 
         }
 
-        return data
+        return table
     }
 
-    const teoriya = [
-        [
 
-        ],
-        []
-    ]
 
-    getData()
+    return (
+        <section className='flex items-center justify-center flex-col gap-10'>
+            <h1 className='text-xl'>Выберите свой аудиторию</h1>
 
-    // const groupsCourse1 = {
-    //     'Аудитория 621': 210737156,
-    //     'группа 12': 53675
-    // };
+            {data && (
+                <div>
 
-    // const groupsCourse2 = {
-    //     'группа 21': 53680,
-    //     'группа 22': 53683,
-    //     'группа 23': 53685
-    // };
+                    <Table>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead></TableHead>
+                                {data.map((aud) => (
+                                    <TableHead key={aud.class}>{aud.class}</TableHead>
+                                ))}
+                            </TableRow>
+                        </TableHeader>
 
-    // const groupsCourse3 = {
-    //     'группа 31': 53686,
-    //     'группа 32': 53687
-    // };
+                        <TableBody >
+                            {buildTable(data)}
+                        </TableBody>
 
-    // const fetchData = () => {
-    //     if (classroomId) {
-    //         // fetch(`https://edu.donstu.ru/api/Rasp?idGroup=${groupId}&i`)
-    //         fetch(`https://edu.donstu.ru/api/Rasp?idAudLine=${classroomId}&i`)
-    //             .then((response) => {
-    //                 return response.json();
-    //             })
-    //             .then((data) => {
-    //                 setData(data);
-    //                 console.log(data);
-    //             })
-    //             .catch(function (error) {
-    //                 alert(
-    //                     'Произошла ошибка: ' +
-    //                     error.message
-    //                 );
-    //             });
+                    </Table>
 
-    //     } else {
-    //         alert('Выберите аудиторию');
-    //     }
-    // }
-
-    // return (
-    //     <section className='flex items-center justify-center flex-col gap-10'>
-    //         <h1 className='text-xl'>Выберите свой аудиторию</h1>
-
-    //         <section>
-    //             <select onChange={(e) => setClassroomId(ClassRoom[e.target.value])}>
-    //                 <option value="">Аудитория</option>
-    //                 {Object.keys(ClassRoom).map((aud) => (
-    //                     <option key={aud} value={aud}>{aud}</option>
-    //                 ))}
-    //             </select>
-    //         </section>
-
-    //         <button className='flex items-center text-white bg-slate-500 px-5 py-2 rounded-xl' onClick={fetchData}>Получить данные</button>
-
-    //         {data && (
-    //             <div>
-
-    //                 <Table>
-    //                     <TableHeader>
-    //                         <TableRow>
-    //                             <TableHead>Номер занятия:</TableHead>
-    //                             <TableHead>День недели:</TableHead>
-    //                             <TableHead>Аудитория:</TableHead>
-    //                             <TableHead>Дата:</TableHead>
-    //                             <TableHead>Дисциплина:</TableHead>
-    //                             <TableHead>Преподаватель:</TableHead>
-    //                         </TableRow>
-    //                     </TableHeader>
-    //                     {data && data.data.rasp.map((item, index) => (
-    //                         <TableBody key={index}>
-    //                             <TableRow>
-    //                                 <TableCell>{item.номерЗанятия} | {item.начало}-{item.конец}</TableCell>
-    //                                 <TableCell>{item.день_недели}</TableCell>
-    //                                 <TableCell>{item.аудитория}</TableCell>
-    //                                 <TableCell>{item.дата}</TableCell>
-    //                                 <TableCell>{item.дисциплина}</TableCell>
-    //                                 <TableCell>{item.преподаватель}</TableCell>
-    //                             </TableRow>
-    //                         </TableBody>
-    //                     ))}
-    //                 </Table>
-
-    //             </div>
-    //         )}
-    //     </section>
-    // )
+                </div>
+            )}
+        </section>
+    )
 
     return (<div></div>)
 }
